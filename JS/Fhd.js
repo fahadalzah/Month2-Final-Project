9007d1a62944e056357315e5f786df848dab6d81
Fahad <fahadsaadalzahrani8@gmail.com>
2023-09-05 17:00:11 +0300

    Finshed the hawaui store part

--- JS/Fhd.js
+++ JS/Fhd.js
@@ -1,12 +1,13 @@
 
 let body1 = document.getElementById('body')
-let h1 = document.createElement('h1')
-h1.innerHTML = 'Apple'
-h1.id = 'h1'
-body1.appendChild(h1)
+let logoA = document.createElement('img')
+logoA.src = '../IMG1/apple (1).png'
+logoA.id = 'logo'
+body1.appendChild(logoA)
 let firstsection = document.createElement('section')
 body1.appendChild(firstsection)
 firstsection.id = 'flex'
+
 
 
 
@@ -346,6 +347,381 @@ firstsection.id = 'flex'
     let imgphone = document.createElement('img')
     dphone.appendChild(imgphone)
     imgphone.src = "../IMG1/clean.png"
+    imgphone.id = 'imgphone'
+
+    let h3 = document.createElement('h3')
+    h3.id = 'h3'
+    h3.innerHTML = 'تنضيف وتلميع الجهاز'
+    dcards.appendChild(h3)
+
+    let h5 = document.createElement('h5')
+    h5.id = 'h5'
+    h5.innerHTML = 'خصم 40% فتره مححدده'
+    dcards.appendChild(h5)
+
+    let dprice = document.createElement('div')
+    dprice.id = 'price'
+    dcards.appendChild(dprice)
+
+    let p1 = document.createElement('p')
+    p1.id = 'p1'
+    p1.innerHTML = '30 ر.س'
+    dprice.appendChild(p1)
+
+    let p2 = document.createElement('p')
+    p2.id = 'p2'
+    dprice.appendChild(p2)
+
+    let pdel = document.createElement('del')
+    p2.appendChild(pdel)
+    pdel.innerHTML = '50 ر.س '
+
+    let cartimg = document.createElement('img')
+    cartimg.src = "../IMG1/shopping-cart.png"
+    cartimg.id = 'cartimg'
+    dprice.appendChild(cartimg)
+}
+let logoH = document.createElement('img')
+logoH.src = '../IMG1/huawei.png'
+logoH.id = 'logoH'
+body1.appendChild(logoH)
+let section2 = document.createElement('section')
+body1.appendChild(section2)
+section2.id = 'flex'
+{
+    let dcards = document.createElement('div')
+    dcards.id = 'cards'
+    section2.appendChild(dcards)
+
+    let dphone = document.createElement('div')
+    dphone.id = 'phone'
+    dcards.appendChild(dphone)
+
+    let imgphone = document.createElement('img')
+    dphone.appendChild(imgphone)
+    imgphone.src = "../IMG1/bbphoneH.png"
+    imgphone.id = 'imgphone'
+
+    let h3 = document.createElement('h3')
+    h3.id = 'h3'
+    h3.innerHTML = ' تصليح الشاشة الخلفيه'
+    dcards.appendChild(h3)
+
+    let h5 = document.createElement('h5')
+    h5.id = 'h5'
+    h5.innerHTML = 'خصم 20% فتره مححدده'
+    dcards.appendChild(h5)
+
+    let dprice = document.createElement('div')
+    dprice.id = 'price'
+    dcards.appendChild(dprice)
+
+    let p1 = document.createElement('p')
+    p1.idsection2 = 'p1'
+    p1.innerHTML = '180 ر.س'
+    dprice.appendChild(p1)
+
+    let p2 = document.createElement('p')
+    p2.id = 'p2'
+    dprice.appendChild(p2)
+
+    let pdel = document.createElement('del')
+    p2.appendChild(pdel)
+    pdel.innerHTML = '200 ر.س '
+
+    let cartimg = document.createElement('img')
+    cartimg.src = "../IMG1/shopping-cart.png"
+    cartimg.id = 'cartimg'
+    dprice.appendChild(cartimg)
+}
+{
+    let dcards = document.createElement('div')
+    dcards.id = 'cards'
+    section2.appendChild(dcards)
+
+    let dphone = document.createElement('div')
+    dphone.id = 'phone'
+    dcards.appendChild(dphone)
+
+    let imgphone = document.createElement('img')
+    dphone.appendChild(imgphone)
+    imgphone.src = "../IMG1/cleanH.png"
+    imgphone.id = 'imgphone'
+
+    let h3 = document.createElement('h3')
+    h3.id = 'h3'
+    h3.innerHTML = 'تنضيف وتلميع الجهاز'
+    dcards.appendChild(h3)
+
+    let h5 = document.createElement('h5')
+    h5.id = 'h5'
+    h5.innerHTML = 'خصم 40% فتره مححدده'
+    dcards.appendChild(h5)
+
+    let dprice = document.createElement('div')
+    dprice.id = 'price'
+    dcards.appendChild(dprice)
+
+    let p1 = document.createElement('p')
+    p1.id = 'p1'
+    p1.innerHTML = '30 ر.س'
+    dprice.appendChild(p1)
+
+    let p2 = document.createElement('p')
+    p2.id = 'p2'
+    dprice.appendChild(p2)
+
+    let pdel = document.createElement('del')
+    p2.appendChild(pdel)
+    pdel.innerHTML = '50 ر.س '
+
+    let cartimg = document.createElement('img')
+    cartimg.src = "../IMG1/shopping-cart.png"
+    cartimg.id = 'cartimg'
+    dprice.appendChild(cartimg)
+}
+{
+    // this is the creation of the card and giving the id for it
+    let dcards = document.createElement('div')
+    dcards.id = 'cards'
+    section2.appendChild(dcards)
+    // this is the creation of the div for the picture 
+    let dphone = document.createElement('div')
+    dphone.id = 'phone'
+    dcards.appendChild(dphone)
+    //this is the creation of the picture and we made it inside the div for it and its src/id
+    let imgphone = document.createElement('img')
+    dphone.appendChild(imgphone)
+    imgphone.src = "../IMG1/BphoneH.png"
+    imgphone.id = 'imgphone'
+    // this is the creation for the heading for the work of the phone
+    let h3 = document.createElement('h3')
+    h3.id = 'h3'
+    h3.innerHTML = " تصليح شاشة الاماميه "
+    dcards.appendChild(h3)
+    let h5 = document.createElement('h5')
+    h5.id = 'h5'
+    h5.innerHTML = ' خصم 37% فتره مححدده'
+    dcards.appendChild(h5)
+
+    let dprice = document.createElement('div')
+    dprice.id = 'price'
+    dcards.appendChild(dprice)
+
+    let p1 = document.createElement('p')
+    p1.id = 'p1'
+    p1.innerHTML = '94.5 ر.س'
+    dprice.appendChild(p1)
+
+    let p2 = document.createElement('p')
+    p2.id = 'p2'
+    dprice.appendChild(p2)
+
+    let pdel = document.createElement('del')
+    p2.appendChild(pdel)
+    pdel.innerHTML = '150 ر.س'
+
+    let cartimg = document.createElement('img')
+    cartimg.src = "../IMG1/shopping-cart.png"
+    cartimg.id = 'cartimg'
+    dprice.appendChild(cartimg)
+}
+{
+    let dcards = document.createElement('div')
+    dcards.id = 'cards'
+    section2.appendChild(dcards)
+
+    let dphone = document.createElement('div')
+    dphone.id = 'phone'
+    dcards.appendChild(dphone)
+
+    let imgphone = document.createElement('img')
+    dphone.appendChild(imgphone)
+    imgphone.src = "../IMG1/no-photoH.png"
+    imgphone.id = 'imgphone'
+
+    let h3 = document.createElement('h3')
+    h3.id = 'h3'
+    h3.innerHTML = 'تصليح الكميرة الاماميه والخلفيه'
+    dcards.appendChild(h3)
+
+    let h5 = document.createElement('h5')
+    h5.id = 'h5'
+    h5.innerHTML = 'خصم 48% فتره مححدده'
+    dcards.appendChild(h5)
+
+    let dprice = document.createElement('div')
+    dprice.id = 'price'
+    dcards.appendChild(dprice)
+
+    let p1 = document.createElement('p')
+    p1.id = 'p1'
+    p1.innerHTML = ' 83.64 ر.س'
+    dprice.appendChild(p1)
+
+    let p2 = document.createElement('p')
+    p2.id = 'p2'
+    dprice.appendChild(p2)
+
+    let pdel = document.createElement('del')
+    p2.appendChild(pdel)
+    pdel.innerHTML = '164 ر.س '
+
+    let cartimg = document.createElement('img')
+    cartimg.src = "../IMG1/shopping-cart.png"
+    cartimg.id = 'cartimg'
+    dprice.appendChild(cartimg)
+}
+{
+    let dcards = document.createElement('div')
+    dcards.id = 'cards'
+    section2.appendChild(dcards)
+
+    let dphone = document.createElement('div')
+    dphone.id = 'phone'
+    dcards.appendChild(dphone)
+
+    let imgphone = document.createElement('img')
+    dphone.appendChild(imgphone)
+    imgphone.src = "../IMG1/batteryH.png"
+    imgphone.id = 'imgphone'
+
+    let h3 = document.createElement('h3')
+    h3.id = 'h3'
+    h3.innerHTML = ' تصليح بطريات'
+    dcards.appendChild(h3)
+
+    let h5 = document.createElement('h5')
+    h5.id = 'h5'
+    h5.innerHTML = 'خصم 7% فتره مححدده'
+    dcards.appendChild(h5)
+
+    let dprice = document.createElement('div')
+    dprice.id = 'price'
+    dcards.appendChild(dprice)
+
+    let p1 = document.createElement('p')
+    p1.id = 'p1'
+    p1.innerHTML = ' 93 ر.س'
+    dprice.appendChild(p1)
+
+    let p2 = document.createElement('p')
+    p2.id = 'p2'
+    dprice.appendChild(p2)
+
+    let pdel = document.createElement('del')
+    p2.appendChild(pdel)
+    pdel.innerHTML = '101 ر.س '
+
+    let cartimg = document.createElement('img')
+    cartimg.src = "../IMG1/shopping-cart.png"
+    cartimg.id = 'cartimg'
+    dprice.appendChild(cartimg)
+}
+{
+    let dcards = document.createElement('div')
+    dcards.id = 'cards'
+    section2.appendChild(dcards)
+
+    let dphone = document.createElement('div')
+    dphone.id = 'phone'
+    dcards.appendChild(dphone)
+
+    let imgphone = document.createElement('img')
+    dphone.appendChild(imgphone)
+    imgphone.src = "../IMG1/updateH.png"
+    imgphone.id = 'imgphone'
+
+    let h3 = document.createElement('h3')
+    h3.id = 'h3'
+    h3.innerHTML = ' تحديث برنامج الجوال'
+    dcards.appendChild(h3)
+
+    let h5 = document.createElement('h5')
+    h5.id = 'h5'
+    h5.innerHTML = 'خصم 50% فتره مححدده'
+    dcards.appendChild(h5)
+
+    let dprice = document.createElement('div')
+    dprice.id = 'price'
+    dcards.appendChild(dprice)
+
+    let p1 = document.createElement('p')
+    p1.id = 'p1'
+    p1.innerHTML = '40 ر.س'
+    dprice.appendChild(p1)
+
+    let p2 = document.createElement('p')
+    p2.id = 'p2'
+    dprice.appendChild(p2)
+
+    let pdel = document.createElement('del')
+    p2.appendChild(pdel)
+    pdel.innerHTML = '80 ر.س '
+
+    let cartimg = document.createElement('img')
+    cartimg.src = "../IMG1/shopping-cart.png"
+    cartimg.id = 'cartimg'
+    dprice.appendChild(cartimg)
+}
+{
+    let dcards = document.createElement('div')
+    dcards.id = 'cards'
+    section2.appendChild(dcards)
+
+    let dphone = document.createElement('div')
+    dphone.id = 'phone'
+    dcards.appendChild(dphone)
+
+    let imgphone = document.createElement('img')
+    dphone.appendChild(imgphone)
+    imgphone.src = "../IMG1/bbphoneH.png"
+    imgphone.id = 'imgphone'
+
+    let h3 = document.createElement('h3')
+    h3.id = 'h3'
+    h3.innerHTML = ' تصليح الشاشة الخلفيه'
+    dcards.appendChild(h3)
+
+    let h5 = document.createElement('h5')
+    h5.id = 'h5'
+    h5.innerHTML = 'خصم 20% فتره مححدده'
+    dcards.appendChild(h5)
+
+    let dprice = document.createElement('div')
+    dprice.id = 'price'
+    dcards.appendChild(dprice)
+
+    let p1 = document.createElement('p')
+    p1.id = 'p1'
+    p1.innerHTML = '180 ر.س'
+    dprice.appendChild(p1)
+
+    let p2 = document.createElement('p')
+    p2.id = 'p2'
+    dprice.appendChild(p2)
+
+    let pdel = document.createElement('del')
+    p2.appendChild(pdel)
+    pdel.innerHTML = '200 ر.س '
+
+    let cartimg = document.createElement('img')
+    cartimg.src = "../IMG1/shopping-cart.png"
+    cartimg.id = 'cartimg'
+    dprice.appendChild(cartimg)
+}
+{
+    let dcards = document.createElement('div')
+    dcards.id = 'cards'
+    section2.appendChild(dcards)
+
+    let dphone = document.createElement('div')
+    dphone.id = 'phone'
+    dcards.appendChild(dphone)
+
+    let imgphone = document.createElement('img')
+    dphone.appendChild(imgphone)
+    imgphone.src = "../IMG1/cleanH.png"
     imgphone.id = 'imgphone'
 
     let h3 = document.createElement('h3')

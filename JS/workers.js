0e225d482528388bb691c9dd55999637f0e7443d
Fahad <fahadsaadalzahrani8@gmail.com>
2023-09-06 18:32:10 +0300

    made the workers page

---
+++ JS/workers.js
@@ -0,0 +1,212 @@
+let body1 = document.getElementById('body1')
+let section1 = document.getElementById('flex')
+{
+    let div1 = document.createElement('div')
+    let div2 = document.createElement('div')
+    let div3 = document.createElement('div')
+    let img1 = document.createElement('img')
+    let span1 = document.createElement('span')
+    let p1 = document.createElement('p')
+    div1.className = 'card1'
+    div2.className = 'card__image'
+    div3.className = 'card__content'
+    span1.className = 'title'
+    flex.appendChild(div1)
+    div1.appendChild(div2)
+    div1.appendChild(div3)
+    div2.appendChild(img1)
+    div3.appendChild(span1)
+    div3.appendChild(p1)
+    p1.innerHTML = 'يبمسا بتسابمنتسماتي بتسيبمسباس تيستياب'
+    span1.innerHTML = 'بتال'
+    img1.src = '../IMG1/user.png'
+    img1.alt = 'profile'
+    let div4 = document.createElement('div')
+    div4.className = 'rating'
+    div1.appendChild(div4)
+    let input1 = document.createElement('input')
+    input1.value = '5'
+    input1.name = 'rating'
+    input1.id = 'star5'
+    input1.type = 'radio'
+    let label1 = document.createElement('label')
+    label1.htmlFor = 'star5'
+    div4.appendChild(input1)
+    div4.appendChild(label1)
+    let input2 = document.createElement('input')
+    input2.value = '4'
+    input2.name = 'rating'
+    input2.id = 'star4'
+    input2.type = 'radio'
+    let label2 = document.createElement('label')
+    label2.htmlFor = 'star4'
+    div4.appendChild(input2)
+    div4.appendChild(label2)
+    let input3 = document.createElement('input')
+    input3.value = '3'
+    input3.name = 'rating'
+    input3.id = 'star3'
+    input3.type = 'radio'
+    let label3 = document.createElement('label')
+    label3.htmlFor = 'star3'
+    div4.appendChild(input3)
+    div4.appendChild(label3)
+    let input4 = document.createElement('input')
+    input4.value = '2'
+    input4.name = 'rating'
+    input4.id = 'star2'
+    input4.type = 'radio'
+    let label4 = document.createElement('label')
+    label4.htmlFor = 'star2'
+    div4.appendChild(input4)
+    div4.appendChild(label4)
+    let input5 = document.createElement('input')
+    input5.value = '1'
+    input5.name = 'rating'
+    input5.id = 'star1'
+    input5.type = 'radio'
+    let label5 = document.createElement('label')
+    label5.htmlFor = 'star1'
+    div4.appendChild(input5)
+    div4.appendChild(label5)
+}
+{
+    let div1 = document.createElement('div')
+    let div2 = document.createElement('div')
+    let div3 = document.createElement('div')
+    let img1 = document.createElement('img')
+    let span1 = document.createElement('span')
+    let p1 = document.createElement('p')
+    div1.className = 'card1'
+    div2.className = 'card__image'
+    div3.className = 'card__content'
+    span1.className = 'title'
+    flex.appendChild(div1)
+    div1.appendChild(div2)
+    div1.appendChild(div3)
+    div2.appendChild(img1)
+    div3.appendChild(span1)
+    div3.appendChild(p1)
+    p1.innerHTML = 'يبمسا بتسابمنتسماتي بتسيبمسباس تيستياب'
+    span1.innerHTML = 'فارس'
+    img1.src = '../IMG1/user.png'
+    img1.alt = 'profile'
+    let div4 = document.createElement('div')
+    div4.className = 'rating1'
+    div1.appendChild(div4)
+    let input1 = document.createElement('input')
+    input1.value = '5'
+    input1.name = 'rating'
+    input1.id = 'star5'
+    input1.type = 'radio'
+    let label1 = document.createElement('label')
+    label1.htmlFor = 'star5'
+    div4.appendChild(input1)
+    div4.appendChild(label1)
+    let input2 = document.createElement('input')
+    input2.value = '4'
+    input2.name = 'rating'
+    input2.id = 'star4'
+    input2.type = 'radio'
+    let label2 = document.createElement('label')
+    label2.htmlFor = 'star4'
+    div4.appendChild(input2)
+    div4.appendChild(label2)
+    let input3 = document.createElement('input')
+    input3.value = '3'
+    input3.name = 'rating'
+    input3.id = 'star3'
+    input3.type = 'radio'
+    let label3 = document.createElement('label')
+    label3.htmlFor = 'star3'
+    div4.appendChild(input3)
+    div4.appendChild(label3)
+    let input4 = document.createElement('input')
+    input4.value = '2'
+    input4.name = 'rating'
+    input4.id = 'star2'
+    input4.type = 'radio'
+    let label4 = document.createElement('label')
+    label4.htmlFor = 'star2'
+    div4.appendChild(input4)
+    div4.appendChild(label4)
+    let input5 = document.createElement('input')
+    input5.value = '1'
+    input5.name = 'rating'
+    input5.id = 'star1'
+    input5.type = 'radio'
+    let label5 = document.createElement('label')
+    label5.htmlFor = 'star1'
+    div4.appendChild(input5)
+    div4.appendChild(label5)
+}
+{
+    let div1 = document.createElement('div')
+    let div2 = document.createElement('div')
+    let div3 = document.createElement('div')
+    let img1 = document.createElement('img')
+    let span1 = document.createElement('span')
+    let p1 = document.createElement('p')
+    div1.className = 'card1'
+    div2.className = 'card__image'
+    div3.className = 'card__content'
+    span1.className = 'title'
+    flex.appendChild(div1)
+    div1.appendChild(div2)
+    div1.appendChild(div3)
+    div2.appendChild(img1)
+    div3.appendChild(span1)
+    div3.appendChild(p1)
+    p1.innerHTML = 'يبمسا بتسابمنتسماتي بتسيبمسباس تيستياب'
+    span1.innerHTML = 'فهد'
+    img1.src = '../IMG1/user.png'
+    img1.alt = 'profile'
+    let div4 = document.createElement('div')
+    div4.className = 'rating2'
+    div1.appendChild(div4)
+    let input1 = document.createElement('input')
+    input1.value = '5'
+    input1.name = 'rating'
+    input1.id = 'star5'
+    input1.type = 'radio'
+    let label1 = document.createElement('label')
+    label1.htmlFor = 'star5'
+    div4.appendChild(input1)
+    div4.appendChild(label1)
+    let input2 = document.createElement('input')
+    input2.value = '4'
+    input2.name = 'rating'
+    input2.id = 'star4'
+    input2.type = 'radio'
+    let label2 = document.createElement('label')
+    label2.htmlFor = 'star4'
+    div4.appendChild(input2)
+    div4.appendChild(label2)
+    let input3 = document.createElement('input')
+    input3.value = '3'
+    input3.name = 'rating'
+    input3.id = 'star3'
+    input3.type = 'radio'
+    let label3 = document.createElement('label')
+    label3.htmlFor = 'star3'
+    div4.appendChild(input3)
+    div4.appendChild(label3)
+    let input4 = document.createElement('input')
+    input4.value = '2'
+    input4.name = 'rating'
+    input4.id = 'star2'
+    input4.type = 'radio'
+    let label4 = document.createElement('label')
+    label4.htmlFor = 'star2'
+    div4.appendChild(input4)
+    div4.appendChild(label4)
+    let input5 = document.createElement('input')
+    input5.value = '1'
+    input5.name = 'rating'
+    input5.id = 'star1'
+    input5.type = 'radio'
+    let label5 = document.createElement('label')
+    label5.htmlFor = 'star1'
+    div4.appendChild(input5)
+    div4.appendChild(label5)
+}
